402a9c9e453bc2e3fef137f934fb0770d7b3c1e0
OleksiiKostenko <kostenko.oleksii91@gmail.com>
2023-03-29 13:03:16 +0300

    add

--- src/components/feedbackMarup/FeedbackOptions.jsx
+++ src/components/feedbackMarup/FeedbackOptions.jsx
@@ -1,18 +1,21 @@
 import PropTypes from 'prop-types';
-import css from 'components/feedbackMarup/FeedbackOptions.module.css'
+import css from 'components/feedbackMarup/FeedbackOptions.module.css';
 
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
-    return (
-        <ul className={css.item}>
-            {options.map(option => (
-        <li className= {css.list} key={option}>
-            <button className= {css.btn} type="button" onClick={onLeaveFeedback}>{option}</button>
+  return (
+    <ul className={css.item}>
+      {options.map(option => (
+        <li className={css.list} key={option}>
+          <button className={css.btn} type="button" onClick={onLeaveFeedback}>
+            {option}
+          </button>
         </li>
-           ))}
-    </ul>)
-}
+      ))}
+    </ul>
+  );
+};
 
 FeedbackOptions.propTypes = {
-    onLeaveFeedback: PropTypes.func.isRequired,
-    options: PropTypes.array.isRequired
-}
+  onLeaveFeedback: PropTypes.func.isRequired,
+  options: PropTypes.array.isRequired,
+};
